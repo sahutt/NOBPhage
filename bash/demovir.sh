e38e90bcb4400d7016e6fc391716b80ca9ad4dc9
sahutt <69019408+sahutt@users.noreply.github.com>
2021-06-17 09:48:18 -0500

    Update demovir.sh

--- bash/demovir.sh
+++ bash/demovir.sh
@@ -1,4 +1,15 @@
-
+#!/bin/sh
+#SBATCH -A p31378
+#SBATCH -p normal
+#SBATCH -N 1
+#SBATCH -n 5
+#SBATCH -t 8:00:00
+#SBATCH --mem=5GB
+#SBATCH --job-name="tb_demovir"
+#SBATCH --mail-user=stefaniehuttelmaier2024@u.northwestern.edu
+#SBATCH --mail-type=ALL
+#SBATCH --output=/projects/b1052/Wells_b1042/Stefanie/toothbrushes/demovir/demovir.out
+#SBATCH --error=/projects/b1052/Wells_b1042/Stefanie/toothbrushes/demovir/demovir.err
 
 #load dependencies required per the demovir git
 module purge all
@@ -14,9 +25,9 @@ cp *.faa /projects/b1052/shared/Demovir
 #move to the folder where all your files and demovir are
 cd /projects/b1052/shared/Demovir
 
- F in *.faa do
-        N=$(basename $F .fa)
-        prodigal -a AA.fasta -i $F -p meta &> /dev/null use
+ for F in *.faa do
+        N=$(basename $F) ;
+        prodigal -a AA.fasta -i $F -p meta 
         ./usearch -ublast AA.fasta -db ./uniprot_trembl.viral.udb -evalue 1e-5 -blast6out trembl_ublast.viral.txt -threads 5 &> /dev/null
         sort -u -k1,1 trembl_ublast.viral.txt > trembl_ublast.viral.u.txt
         rm AA.fasta
