aa0a621fd70fc4e675e0cf59a973f9603802c019
sahutt <69019408+sahutt@users.noreply.github.com>
2021-06-17 09:11:36 -0500

    Update demovir.sh

--- bash/demovir.sh
+++ bash/demovir.sh
@@ -1,12 +1,33 @@
 
 
+#load dependencies required per the demovir git
 module purge all
 module load prodigal
+module load R
+module load bzip2
 
 #move to folder with compiled vibrant nucleotide files '_scaffolds.phages_combined.faa'
 #use vibrant_parse.sh to move these files
 cd /projects/b1052/Wells_b1042/Stefanie/toothbrushes/VIBRANT/metaspades/vibrant_results
 #copy all .faa files to the folder containing the demovir scripts
 cp *.faa /projects/b1052/shared/Demovir
+#move to the folder where all your files and demovir are
+cd /projects/b1052/shared/Demovir
+
+ F in *.faa do
+        N=$(basename $F .fa)
+        prodigal -a AA.fasta -i $F -p meta &> /dev/null use
+        ./usearch -ublast AA.fasta -db ./uniprot_trembl.viral.udb -evalue 1e-5 -blast6out trembl_ublast.viral.txt -threads 5 &> /dev/null
+        sort -u -k1,1 trembl_ublast.viral.txt > trembl_ublast.viral.u.txt
+        rm AA.fasta
+        cut -f 1,2 trembl_ublast.viral.u.txt | sed 's/_[0-9]\+\t/\t/' | cut -f 1 | paste trembl_ublast.viral.u.txt - > trembl_ublast.viral.u.contigID.txt
+        rm trembl_ublast.viral.u.txt trembl_ublast.viral.txt
+        Rscript ./demovir_edit.R
+        rm trembl_ublast.viral.u.contigID.txt
+        cp DemoVir_assignments.txt $N_DemoVir_assignments.txt
+        mv $N_DemoVir_assignments.txt /projects/b1052/Wells_b1042/Stefanie/toothbrushes/demovir
+done
+
+
 
 
