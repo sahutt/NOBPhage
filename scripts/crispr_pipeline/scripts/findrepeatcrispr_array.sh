7b47476ffa3163967f22e40ea2e498da025e237e
sahutt <69019408+sahutt@users.noreply.github.com>
2021-11-01 15:43:37 -0500

    Update findrepeatcrispr_array.sh

--- scripts/crispr_pipeline/scripts/findrepeatcrispr_array.sh
+++ scripts/crispr_pipeline/scripts/findrepeatcrispr_array.sh
@@ -2,7 +2,7 @@
 #SBATCH -J findRepeatsCRISPR_array
 #SBATCH -A p31378
 #SBATCH --mail-type=ALL
-#SBATCH --mail-user=stefaniehuttelmaier2024@u.northwestern.edu
+#SBATCH --mail-user=@u.northwestern.edu
 #SBATCH -N 1
 #SBATCH -n 5
 #SBATCH --mem=20GB
@@ -12,19 +12,20 @@
 #SBATCH -p short
 #SBATCH --array=1-97%5
 
+##to subbmit an array, you need to add the --array flag to your header. Submit with the exact number of jobs you have to run
+## in this case, we had 97. The %5 tells the job assigner to submit 5 jobs at a time. The numbers of nodes requested must match
+##the number of jobs submitted at once. 
+
 module purge all
 module load python/anaconda
 
-IFS=$'\n' read -r input_args < python_commands2.txt
+#create tab delimited test file containing the DRs in one column and the output pathway in the second column. Store this text file 
+#as a variable.
+param_store=/projects/p31378/comammox/results/crispr2/pooled/python_commands.txt
 
-echo $input_args
+#link each line in your stored text document to a slurm array task ID.
+param_a=$(cat $param_store | awk -v var=$SLURM_ARRAY_TASK_ID 'NR==var {print $0}')
 
-python /projects/p31378/comammox/scripts/findRepeatCRISPR.py $input_args
+python /projects/p31378/comammox/scripts/findRepeatCRISPR.py $param_a
 
-
-
-#while IFS= read -r line; do
-#	${line[$SLURM_ARRAY_TASK_ID]}	
-#    	echo "$line"
-#done < "python_commands.txt"
 
