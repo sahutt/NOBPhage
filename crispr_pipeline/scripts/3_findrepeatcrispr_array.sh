596ee1101ae82a177f412e8574a1cb43a3d70467
sahutt <69019408+sahutt@users.noreply.github.com>
2021-11-16 15:22:53 -0600

    Update and rename findrepeatcrispr_array.sh to 3_findrepeatcrispr_array.sh

--- crispr_pipeline/scripts/3_findrepeatcrispr_array.sh
+++ crispr_pipeline/scripts/3_findrepeatcrispr_array.sh
@@ -5,14 +5,18 @@
 #SBATCH --mail-user=@u.northwestern.edu
 #SBATCH -N 1
 #SBATCH -n 5
-#SBATCH --mem=20GB
-#SBATCH -t 3:00:00
+#SBATCH --mem=2GB
+#SBATCH -t 6:00:00
 #SBATCH --output=/projects/p31378/comammox/results/crispr2/pooled/findRepeats_%A_%a.out
 #SBATCH --error=/projects/p31378/comammox/results/crispr2/pooled/findRepeats_%A_%a.err
 #SBATCH -p short
 #SBATCH --array=1-97%5
 
-##to subbmit an array, you need to add the --array flag to your header. Submit with the exact number of jobs you have to run
+##note: Several jobs timed out with a lower initial starting time, and about 8 of the 97 still timed out at 6 hours. The array was run a second time
+## for the timed out jobs for 12 hours and all jobs completed. You could start with a higher run time to avoid time outs, but it might increase your
+## wait time before the job starts. 
+
+##to submit an array, you need to add the --array flag to your header. Submit with the exact number of jobs you have to run
 ## in this case, we had 97. The %5 tells the job assigner to submit 5 jobs at a time. The numbers of nodes requested must match
 ##the number of jobs submitted at once. 
 
