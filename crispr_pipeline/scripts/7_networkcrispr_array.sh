1343aac2816f81f588bde76236d51589b518b841
sahutt <69019408+sahutt@users.noreply.github.com>
2021-11-23 12:23:15 -0600

    Update 7_networkcrispr_array.sh

--- crispr_pipeline/scripts/7_networkcrispr_array.sh
+++ crispr_pipeline/scripts/7_networkcrispr_array.sh
@@ -19,4 +19,8 @@ param_store=./networkcrispr_commands.txt
 param_a=$(cat $param_store | awk -v var=$SLURM_ARRAY_TASK_ID 'NR==var {print $2}')
 param_b=$(cat $param_store | awk -v var=$SLURM_ARRAY_TASK_ID 'NR==var {print $1}')
 
+
+#inputs 1) call python 2) path to python script 3) the direct repeat sequence
+#4) the .fasta file output by mergelengthCRISPR.py 5) the .names file output by mergelengthCRISPR.py
+#6) output file name - DR#.network
 python /projects/p31378/comammox/scripts/networkCRISPR.py $param_a /projects/p31378/comammox/results/crispr2/pooled/$param_b.mergeLC.fasta /projects/p31378/comammox/results/crispr2/pooled/$param_b.mergeLC.names /projects/p31378/comammox/results/crispr2/pooled/$param_b.network
