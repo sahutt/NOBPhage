7dafde4d3fb38b390c77b04084de92a832d419a2
sahutt <69019408+sahutt@users.noreply.github.com>
2021-11-23 12:16:46 -0600

    Update 7_networkcrispr_array.sh

--- crispr_pipeline/scripts/7_networkcrispr_array.sh
+++ crispr_pipeline/scripts/7_networkcrispr_array.sh
@@ -19,4 +19,4 @@ param_store=./networkcrispr_commands.txt
 param_a=$(cat $param_store | awk -v var=$SLURM_ARRAY_TASK_ID 'NR==var {print $2}')
 param_b=$(cat $param_store | awk -v var=$SLURM_ARRAY_TASK_ID 'NR==var {print $1}')
 
-python /projects/p31378/comammox/scripts/networkCRISPR.py $param_a /projects/p31378/comammox/results/crispr2/pooled/$param_b.mergeLC.fasta /projects/p31378/comammox/results/crispr2/pooled/$param_b.mergeLC.names $param_b.network
+python /projects/p31378/comammox/scripts/networkCRISPR.py $param_a /projects/p31378/comammox/results/crispr2/pooled/$param_b.mergeLC.fasta /projects/p31378/comammox/results/crispr2/pooled/$param_b.mergeLC.names /projects/p31378/comammox/results/crispr2/pooled/$param_b.network
