171657162a978e5b1f8ada7a3774280f432b5b3d
sahutt <69019408+sahutt@users.noreply.github.com>
2022-01-18 13:45:14 -0600

    Update 3_bbduk_filtering.sh

--- crispr_pipeline/scripts/3_bbduk_filtering.sh
+++ crispr_pipeline/scripts/3_bbduk_filtering.sh
@@ -18,6 +18,19 @@ cd /projects/p31378/bbmap_38.94
 # before running bbduk, make sure that the k-mer length (k=#) is equal to the length of the shortest direct repeat
 # this is a column in the crisprreports.tsv files and the summary.csv
 
+
+#BBDuk flags:
+# - Xmx18g - disables cap on memory, BBDuk automatically limits memory usage to half of availabe
+# the 18g in this flag tells BBDuk to use 18gb, or just under the 20gb alloted
+# -eoom - no clue what this is
+# in,in2 - fastq files that will be searched for the target sequences
+#outm, outm2 - fastq outputs that contain the reads that matched to you query
+# Note: if you want the unmatched files, use flags out, out2
+# k=23 - the kmer length that must match  
+# mm=f - this disables 'maskmiddle' which inores the middle base of a kmer. This only allows for exact matches
+# hdist=1 - hamming number? stores more kmers the higher this gets? 
+#rcomp-T - this matches forward and and reverse complements, set to f if you only want matches in the forward orientation
+
 # s16
 ./bbduk.sh -Xmx18g -eoom \
 in=/projects/p31378/comammox/raw_reads/SG_05_27_2016_S16_trimmed_1.fastq.gz \
