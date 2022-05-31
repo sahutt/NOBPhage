701ed5cc87331f2c2548f2c074fd394f2860a9bc
unknown <mckennafarmer2023@u.northwestern.edu>
2022-05-31 14:50:59 -0500

    Delete 1_findrepeatCrispr.sh

--- crispr_take2/scripts/1_findrepeatCrispr.sh
+++
@@ -1,6 +0,0 @@
-
-
-##Crispr_take2 pipeline uses the 3 BBduk alignment fastq outputs, 1 per sampling date, from the original crispr_pipeline. 
-##However, rather than using an array to split the findrepeatCRISPR.py outputs by unique DR, here they are output by date and
-##one output includes all reads for a total of 4 groups moving through the same pipeline. 
-
